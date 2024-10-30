3d3b188a016abcae21cbdd3f1210b2f6b06186db
Kaisentlaia <kaisentlaia@gmail.com>
2024-10-30 20:45:50 +0100

    added tooltips and fixed a few labels

--- KsRealisticFarming/KsRealisticFarmingModSystem.cs
+++ KsRealisticFarming/KsRealisticFarmingModSystem.cs
@@ -3,6 +3,8 @@ using HarmonyLib;
 using Vintagestory.GameContent;
 using Vintagestory.API.Server;
 using Vintagestory.API.Client;
+using Vintagestory.API.Util;
+using System.Linq;
 
 namespace KsRealisticFarming;
 
@@ -18,14 +20,6 @@ public class KsRealisticFarmingModSystem : ModSystem
         {
             base.Start(api);
             CoreAPI = api;
-        }   
-        
-        /// <summary>
-        /// Server-specific intialization
-        /// </summary>
-        public override void StartServerSide(ICoreServerAPI api)
-        {
-            CoreServerAPI = api;
             if (!Harmony.HasAnyPatches(Mod.Info.ModID)) {
                 harmony = new Harmony(Mod.Info.ModID);
                 harmony.PatchAll(); // Applies all harmony patches
@@ -33,9 +27,60 @@ public class KsRealisticFarmingModSystem : ModSystem
         }
 
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(BlockCrop), "OnLoaded")]
+        [HarmonyPatch(typeof(BlockCrop), nameof(BlockCrop.OnLoaded))]
         public static void PreOnLoaded() {
             BlockCrop.WildCropDropMul = 0.5f;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(BlockCrop), "GetPlacedBlockInteractionHelp")]
+        public static WorldInteraction[] PostGetPlacedBlockInteractionHelp(WorldInteraction[] __result, BlockCrop __instance)
+        {
+            bool isVegetable = __instance.Code.Path.Contains("cabbage") ||
+                __instance.Code.Path.Contains("carrot") ||
+                __instance.Code.Path.Contains("turnip") ||
+                __instance.Code.Path.Contains("cassava") ||
+                __instance.Code.Path.Contains("onion") ||
+                __instance.Code.Path.Contains("parsnip");
+            WorldInteraction[] interactions;
+            if (isVegetable) {
+                interactions = new WorldInteraction[3] {
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "ksrealisticfarming:blockhelp-crop-breaktoharvest-partial",
+                        MouseButton = EnumMouseButton.Left,
+                        ShouldApply = (WorldInteraction wi, BlockSelection bs, EntitySelection es) => __instance.CurrentCropStage == __instance.CropProps.GrowthStages - 2
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "blockhelp-crop-breaktoharvest",
+                        MouseButton = EnumMouseButton.Left,
+                        ShouldApply = (WorldInteraction wi, BlockSelection bs, EntitySelection es) => __instance.CurrentCropStage == __instance.CropProps.GrowthStages - 1
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "ksrealisticfarming:blockhelp-crop-breaktoharvest-seeds",
+                        MouseButton = EnumMouseButton.Left,
+                        ShouldApply = (WorldInteraction wi, BlockSelection bs, EntitySelection es) => __instance.CurrentCropStage == __instance.CropProps.GrowthStages
+                    },
+                };
+            } else {
+                interactions = new WorldInteraction[2] {
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "ksrealisticfarming:blockhelp-crop-breaktoharvest-partial",
+                        MouseButton = EnumMouseButton.Left,
+                        ShouldApply = (WorldInteraction wi, BlockSelection bs, EntitySelection es) => __instance.CurrentCropStage == __instance.CropProps.GrowthStages - 1
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "blockhelp-crop-breaktoharvest",
+                        MouseButton = EnumMouseButton.Left,
+                        ShouldApply = (WorldInteraction wi, BlockSelection bs, EntitySelection es) => __instance.CurrentCropStage == __instance.CropProps.GrowthStages
+                    }
+                };
+            }
+            return interactions.Append(__result.Remove(__result.First()));
         }
 
         /// <summary>
